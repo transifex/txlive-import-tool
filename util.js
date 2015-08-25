df52a5cf7bbf1767bc99df333edf50b92593d3a1
Nikos Vasileiou <nikosv@transifex.com>
2015-08-25 11:46:25 +0300

    Refactor exporter to match strings using xpath

--- util.js
+++ util.js
@@ -21,21 +21,41 @@ function runPhantomJs(url, callback) {
       if (stderr) console.error(stderr);
       return;
     }
-    //get json by get stdout part of [LIVE_EXPORT_BEGIN] and [LIVE_EXPORT_END]
-    var begin_tag = '[LIVE_EXPORT_BEGIN]';
-    var end_tag = '[LIVE_EXPORT_END]';
-    var begin_index = stdout.indexOf(begin_tag) + begin_tag.length;
-    var end_index = stdout.indexOf(end_tag);
+
+    //get strings json by get stdout part of [LIVE_EXPORT_BEGIN] and [LIVE_EXPORT_END]
+    var export_begin_tag = '[LIVE_EXPORT_BEGIN]';
+    var export_end_tag = '[LIVE_EXPORT_END]';
+    var export_begin_index = stdout.indexOf(export_begin_tag) + export_begin_tag.length;
+    var export_end_index = stdout.indexOf(export_end_tag);
+    var strings;
     try {
-      var result = JSON.parse(
-        stdout.substr(begin_index, end_index - begin_index)
+      strings = JSON.parse(
+        stdout.substr(export_begin_index, export_end_index - export_begin_index)
       );
-      callback(result);
     }
     catch(error) {
       console.error(stdout);
       console.error(error);
+      return;
     }
+
+    //get xpath json by get stdout part of [LIVE_XPATH_BEGIN] and [LIVE_XPATH_END]
+    var xpath_begin_tag = '[LIVE_XPATH_BEGIN]';
+    var xpath_end_tag = '[LIVE_XPATH_END]';
+    var xpath_begin_index = stdout.indexOf(xpath_begin_tag) + xpath_begin_tag.length;
+    var xpath_end_index = stdout.indexOf(xpath_end_tag);
+    var xpaths;
+    try {
+      xpaths = JSON.parse(
+        stdout.substr(xpath_begin_index, xpath_end_index - xpath_begin_index)
+      );
+    }
+    catch(error) {
+      console.error(stdout);
+      console.error(error);
+      return;
+    }
+    callback(strings, xpaths);
   });
 }
 
